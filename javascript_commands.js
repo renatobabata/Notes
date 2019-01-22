15ff221863306d807a91d2bb99dcbec30bba1e71
Renato Babata <37925829+renatobabata@users.noreply.github.com>
2019-01-22 15:56:57 -0200

    Update javascript_commands.js

--- javascript_commands.js
+++ javascript_commands.js
@@ -205,6 +205,25 @@ Dog.prototype = {
 };
 
 
+//Prototype Inheritance
+//All JavaScript objects inherit properties and methods from a prototype:
+//Date objects inherit from Date.prototype
+//Array objects inherit from Array.prototype
+//Person objects inherit from Person.prototype
+//The Object.prototype is on the top of the prototype inheritance chain:
+//Date objects, Array objects, and Person objects inherit from Object.prototype.
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+Person.prototype.nationality = "English";
+
+
+
+
+
 
 
 
