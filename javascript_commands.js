6234833516df48e16d37e9d3b70aa02fdb6823d9
Renato Babata <37925829+renatobabata@users.noreply.github.com>
2019-02-28 14:29:47 -0300

    Update javascript_commands.js

--- javascript_commands.js
+++ javascript_commands.js
@@ -380,3 +380,44 @@ function destroyer(arr) {
   });
 }
 
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+function whatIsInAName(collection, source) {
+  var srcKeys = Object.keys(source);
+  // filter the collection
+  return collection.filter(function (obj) {
+    return srcKeys
+      .map(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      })
+      .reduce(function(a, b) {
+        return a && b;
+      });
+  });
+}
+// test here
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
